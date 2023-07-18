cdb378f4434eef845d4c9937cdc45b7c657b130b
Glauber <glauber.bannwart@gmail.com>
2023-07-18 16:51:06 -0300

    Add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+import '@/styles/app.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import TodoApp from '@/components/TodoApp';
+
+const domContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(domContainer);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <TodoApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
