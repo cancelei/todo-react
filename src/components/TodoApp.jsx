ac211f5e468afaeb23c27417a4f0d6a50a077437
Glauber <glauber.bannwart@gmail.com>
2023-07-18 16:50:51 -0300

    Add Various Components

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "@/routes/Home";
+import About from "@/routes/About";
+import Login from "@/routes/Login";
+import Profile from "@/routes/Profile";
+import Layout from "@/components/Layout";
+import NotMatch from '@/routes/NotMatch';
+
+const TodoApp = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+
+        {/* 404 page */}
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
+  );
+};
+export default TodoApp;
